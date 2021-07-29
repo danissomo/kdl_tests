a7118b730e9c2d3681b2178fb2b7b59675f8c0a4
dan <dan.2k64@gmail.com>
2021-07-29 18:16:54 +0300

    вычисление вектора моментов инерции

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ n=6
 
 q=kdl.JntArray(n)
 qd_prev=kdl.JntArray(n)
-ddq=kdl.JntArray(n)
+qdd=kdl.JntArray(n)
 
 
 grav_torques=kdl.JntArray(n)
@@ -23,11 +23,10 @@ grav=kdl.Vector(0, 0, -9.82)
 for i in range(n-1):
     qd_prev[i]=0.5
     q[i] = 0.5
+
+start_calc=time.time()
 dyn_model=kdl.ChainDynParam(chain, grav)
 
-dyn_model.JntToGravity(q, grav_torques)
-dyn_model.JntToMass(q, mass_matrix)
-dyn_model.JntToCoriolis(q, qd_prev, coriolis_vector)
 
 print(f"mass_matrix_1")
 for i in range(n):
@@ -37,64 +36,35 @@ for i in range(n):
 print()
 
 for i in range(n-1):
-    qd_prev[i]=0.5
-    q[i] += qd_prev[i]*0.004+0.06
-start_calc=time.time()
-
-prev_grav = kdl.JntArray( grav_torques)
-prev_mass_matrix =kdl.JntSpaceInertiaMatrix(mass_matrix)
-prev_corriolis_vector = kdl.JntArray(coriolis_vector)
+    qd_prev[i]+= 0.024
+    qdd[i] = 0.024/0.008
+    q[i] += qd_prev[i]*0.008 +  qdd[i]*0.008*0.008/2
 
 
-dyn_model.JntToGravity(q, grav_torques)
 dyn_model.JntToMass(q, mass_matrix)
-dyn_model.JntToCoriolis(q, qd_prev, coriolis_vector)
-duration=time.time() - start_calc
 
-print(f"mass_matrix_2")
+print(f"mass_matrix_1")
 for i in range(n):
     for j in range(n):
         print( "{:.9f}".format(mass_matrix[i,j]), ",", end = ' ')
     print()
 print()
-dif_grav = kdl.JntArray(n)
-dif_mass =kdl.JntSpaceInertiaMatrix(n)
-dif_coriolis =kdl.JntArray(n)
+result_vec =kdl.JntArray(n)
+kdl.Multiply(mass_matrix, qdd, result_vec)
+
+print(f"tau_a")
 for i in range(n):
-    dif_grav[i] = grav_torques[i]- prev_grav[i]
-    dif_coriolis[i] = coriolis_vector[i] - prev_corriolis_vector[i]
-    for j in range(n):
-        dif_mass[i,j] = mass_matrix[i,j] - prev_mass_matrix[i,j]
+        print( "{:.9f}".format(result_vec[i]), ",", end = ' ')
+print()
 
-
-
+duration = time.time() - start_calc
 
 print(f"q")
 for i in range(n):
     print( q[i], ",",  end=' ')
 print(f"\n")
 
-print(f"qd")
-for i in range(n):
-    print( qd_prev[i], ",", end =' ')
-print(f"\n")
 
-print(f"delta_coriolis_vector")
-for i in range(n):
-    print( "{:.9f}".format(dif_coriolis[i]), ",", end = ' ')
-print(f"\n")
-
-print(f"delta_grav_trq")
-for i in range(n):
-    print( "{:.9f}".format(dif_grav[i]), ",", end = ' ')
-print(f"\n")
-
-print(f"delta_mass_matrix")
-for i in range(n):
-    for j in range(n):
-        print( "{:.9f}".format(dif_mass[i,j]), ",", end = ' ')
-    print()
-print()
 
 print(f"calc_duration\n", duration)
 print(f"----\n\n")
